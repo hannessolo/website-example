c29089eab904b8a10ed6c8adc76f0633026b658e
Hannes Hertach <hanneshertach@gmail.com>
2023-07-27 15:13:41 +0200

    add balloon image and score

--- balloon.js
+++ balloon.js
@@ -1,32 +1,39 @@
 let distanceToTop = 500;
 let distanceToSide = 250;
+let score = 0;
+
+let balloon;
 
 function setup() {
   createCanvas(500, 500);
   ellipseMode(CENTER);
+  balloon = loadImage('./balloon.png');
 }
 
 function draw() {
-  background('blue');
+  background('skyblue');
   fill('green');
-  ellipse(distanceToSide, distanceToTop, 20, 30);
+  image(balloon, distanceToSide, distanceToTop, 40, 60);
+  text('Score: ' + score, 20, 20);
 }
 
 function moveBalloon() {
-  distanceToTop -= 1;
+  distanceToTop -= 2;
 
   if (distanceToTop < 0) {
     distanceToTop = 500;
+    score -= 1;
   }
 }
 
 setInterval(moveBalloon, 10);
 
 function mouseClicked() {
-  if (mouseX > distanceToSide - 10 && mouseX < distanceToSide + 10) {
-    if (mouseY > distanceToTop - 15 && mouseY < distanceToTop + 15) {
+  if (mouseX > distanceToSide && mouseX < distanceToSide + 40) {
+    if (mouseY > distanceToTop && mouseY < distanceToTop + 60) {
       distanceToSide = random(0, 250);
       distanceToTop = 500;
+      score += 1;
     }
   }
 }
